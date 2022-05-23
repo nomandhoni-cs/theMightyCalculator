80a370dc318cc64a718fc047d2c91698dde7a66e
nomandhoni-cs <nomandhoni@yahoo.com>
2022-05-23 18:44:50 +0600

    Implimented clear button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,94 @@
-const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelector('.calculator__keys')
+const one = document.getElementById("One"); 
+const two = document.getElementById("Two");
+const three = document.getElementById("Three"); 
+const four = document.getElementById("Four");
+const five = document.getElementById("Five");
+const six = document.getElementById("Six");
+const seven = document.getElementById("Seven");
+const eight = document.getElementById("Eight");
+const nine = document.getElementById("Nine");
+const zero = document.getElementById("Zero");
+const dot = document.getElementById("Dot");
+const add = document.getElementById("Plus");
+const sub = document.getElementById("Minus");
+const mul = document.getElementById("Multiply");
+const div = document.getElementById("Divide");
+const equals = document.getElementById("numberArea");
+const equal = document.getElementById("Equals");
+const clear = document.getElementById('clear');
 
-keys.addEventListener('click', e => {
- if (e.target.matches('button')) {
-   console.log(e.target.matches + ' clicked');
- }
-})
+clear.onclick = () => {
+  equals.value = '';
+}
+one.onclick = function(){
+    equals.value += 1;
+}
+
+two.onclick = function(){
+    equals.value += 2;
+}
+
+
+three.onclick = function(){
+    equals.value += 3;
+}
+
+four.onclick = function(){
+    equals.value += 4;
+}
+
+five.onclick = function(){
+    equals.value += 5;
+}
+
+six.onclick = function(){
+    equals.value += 6;
+}
+
+seven.onclick = function(){
+    equals.value += 7;
+}
+
+
+eight.onclick = function(){
+    equals.value += 8;
+}
+
+
+nine.onclick = function(){
+    equals.value += 9;
+}
+
+
+zero.onclick = function(){
+    equals.value += 0;
+}
+
+
+add.onclick = function(){
+    equals.value += "+";
+}
+
+
+sub.onclick = function(){
+    equals.value += "-";
+}
+
+
+mul.onclick = function(){
+    equals.value += "*";
+}
+
+
+div.onclick = function(){
+    equals.value += "/";
+}
+
+
+dot.onclick = function(){
+    equals.value += ".";
+}
+
+equal.onclick = function(){
+    equals.value = eval(equals.value);
+}
